6db64ef5bf6b887f5196ec2ea663ff785a1e02b1
germanrogu <german.rodriguezgu@gmail.com>
2023-09-04 15:53:40 -0500

    refactor payment result

--- src/pages/PaymentResult.jsx
+++ src/pages/PaymentResult.jsx
@@ -1,16 +1,43 @@
 import React from "react";
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import "../styles/PaymentResult.css";
+
 function PaymentResult() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const transactionResult = searchParams.get("message");
 
+  const { paymentsCompleted, cart } = useSelector(
+    (state) => state.clientProgress
+  );
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleReset = () => {
+    dispatch({ type: "RESET_PAYMENT_DATA" });
+    dispatch({ type: "RESET_CONFIRMATION_SUCCESS" });
+    navigate("/");
+  };
+
   return (
     <div className='payment-result'>
-      <h2>Resultado de la Transacción</h2>
+      <h2>Transaction Result</h2>
       <p>{transactionResult}</p>
-      <Link to='/'>Volver a comprar</Link>
+      <div>
+        <h3>Order Details:</h3>
+        <ul>
+          {cart.map((item) => (
+            <li key={item.id}>
+              {item.name} - Price: ${item.price}
+            </li>
+          ))}
+        </ul>
+        <p>Payments completed: {paymentsCompleted}</p>
+      </div>
+
+      <button onClick={handleReset}>Repurchase</button>
     </div>
   );
 }
