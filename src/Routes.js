2b18199724abdf8b7fd660cbac5689559b6ab966
germanrogu <german.rodriguezgu@gmail.com>
2023-09-04 16:28:39 -0500

    Add not found component

--- src/Routes.js
+++ src/Routes.js
@@ -3,12 +3,14 @@ import React from "react";
 import { Route, Routes as RouterRoutes } from "react-router-dom";
 import App from "./App";
 import PaymentResult from "./pages/PaymentResult";
+import NotFound from "./pages/NotFound";
 
 function Routes() {
   return (
     <RouterRoutes>
       <Route path='/' element={<App />} />
       <Route path='/payment-result' element={<PaymentResult />} />
+      <Route path='*' element={<NotFound />} />
     </RouterRoutes>
   );
 }
