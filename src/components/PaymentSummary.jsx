f91dee98ac32d28b8e9a25abf05590c767d62eae
germanrogu <german.rodriguezgu@gmail.com>
2023-09-04 15:53:29 -0500

    refactor paymetn summary component

--- src/components/PaymentSummary.jsx
+++ src/components/PaymentSummary.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import "../styles/PaymentSummary.css";
-function PaymentSummary({ paymentInfo, onConfirmPayment }) {
+import { useSelector } from "react-redux";
+
+function PaymentSummary({ onConfirmPayment, onClose }) {
+  const { paymentData, invoiceNumber, receiptVoucher, cart } = useSelector(
+    (state) => state.clientProgress
+  );
+
   return (
-    <div className='payment-summary'>
-      <h2>Resumen del Pago</h2>
-      <p>Número de tarjeta: {paymentInfo.number}</p>
-      <p>Nombre del titular: {paymentInfo.name}</p>
-      <p>Fecha de expiración: {paymentInfo.expiry}</p>
-      <p>CVC: {paymentInfo.cvc}</p>
-      <button onClick={onConfirmPayment}>Confirmar Pago</button>
+    <div className='payment-summary-backdrop'>
+      <div className='payment-summary'>
+        <h2>Payment Summary</h2>
+        <div className='invoice-info'>
+          <p>Card Number: {paymentData.number}</p>
+          <p>Name: {paymentData.name}</p>
+          <p>Expire Date: {paymentData.expiry}</p>
+          <p>CVV: {paymentData.cvv}</p>
+        </div>
+        <hr />
+        <div className='invoice-details'>
+          <p>Invoice Number: {invoiceNumber}</p>
+          <p>Receipt Voucher: {receiptVoucher}</p>
+        </div>
+        <hr />
+        <div className='cart-details'>
+          <h3>Order Details:</h3>
+          <ul>
+            {cart.map((item) => (
+              <li key={item.id}>
+                {item.name} - Price: ${item.price}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <button onClick={onConfirmPayment}>Confirm</button>
+        <button onClick={onClose} className='close-button'>
+          x
+        </button>
+      </div>
     </div>
   );
 }
