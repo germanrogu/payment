285c039b5db83dc926883279f2af356f32486b06
germanrogu <german.rodriguezgu@gmail.com>
2023-09-04 15:54:42 -0500

    refactor sagas to handle request

--- src/store/sagas.js
+++ src/store/sagas.js
@@ -1,13 +1,27 @@
-import { put, takeEvery, call } from "redux-saga/effects";
-import { fetchClientProgress } from "../services/fetchClientProgress";
+import { put, call, takeLatest } from "redux-saga/effects";
+import { processPayment } from "../services/PaymentServices";
 
-function* loadClientProgress() {
+function* confirmPayment(action) {
   try {
-    const clientProgress = yield call(fetchClientProgress);
-    yield put({ type: "UPDATE_CLIENT_PROGRESS", payload: clientProgress });
-  } catch (error) {}
+    const paymentResult = yield call(processPayment, action.payload);
+
+    if (paymentResult.success) {
+      yield put({ type: "UPDATE_CLIENT_PROGRESS", payload: paymentResult });
+      yield put({ type: "PAYMENTS_COMPLETED" });
+      yield put({ type: "CONFIRM_PAYMENT_SUCCESS", payload: paymentResult });
+      yield put({ type: "SET_LOADING", payload: false });
+    } else {
+      console.error("Error en el pago:", paymentResult.message);
+      yield put({ type: "CONFIRM_PAYMENT_FAILURE", payload: paymentResult });
+      yield put({ type: "SET_LOADING", payload: false });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    yield put({ type: "CONFIRM_PAYMENT_FAILURE", payload: error.message });
+    yield put({ type: "SET_LOADING", payload: false });
+  }
 }
 
 export default function* rootSaga() {
-  yield takeEvery("LOAD_CLIENT_PROGRESS", loadClientProgress);
+  yield takeLatest("CONFIRM_PAYMENT", confirmPayment);
 }
